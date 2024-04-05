22531e6f9f703527ed5ba1b4af3338ac5976ea09
vishwas babar <vishwasbabar886@gmail.com>
2024-04-05 21:01:20 +0530

    api key for tiny mce

--- src/components/TmceEditor.jsx
+++ src/components/TmceEditor.jsx
@@ -13,7 +13,7 @@ function TmceEditor({ className, ...props }) {
   return (
     <div className={className}>
       <Editor
-        // apiKey="s9iejl90ug9ofv0k8y8dlz4ydhh7v97mwy5x3r0w9hmgsaw8"
+        apiKey={import.meta.env.VITE_APP_API_URL.toString()}
         onInit={(evt, editor) => (editorRef.current = editor)}
         {...props}
         onChange={log}
